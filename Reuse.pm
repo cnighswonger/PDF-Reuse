4e6ae25cbe6892a96af8296ccbe31c7181aa4404
Chris Nighswonger <cnighswonger@foundations.edu>
2014-12-03 22:38:25 -0500

    Updating copyright info

--- Reuse.pm
+++ Reuse.pm
@@ -2995,12 +2995,14 @@ specific task with support from the Electoral Enrolment Centre, Wellington, New 
 =head1 AUTHOR
 
 Lars Lundberg larslund@cpan.org
+Chris Nighswonger cnighs@cpan.org
 
 =head1 COPYRIGHT
 
 Copyright (C) 2003 - 2004 Lars Lundberg, Solidez HB.
 Copyright (C) 2005 Karin Lundberg.
-Copyright (C) 2006 - Lars Lundberg, Solidez HB.
+Copyright (C) 2006 - 2010 Lars Lundberg, Solidez HB.
+Copyright (C) 2010 - 2014 Chris Nighswonger
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
