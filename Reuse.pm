c661ae678b89e08ba07e8cbe5323792e8d1aef39
Chris Nighswonger <cnighswonger@foundations.edu>
2014-12-08 16:02:32 -0500

    Bumping version number for 0.36 stable

--- Reuse.pm
+++ Reuse.pm
@@ -15,7 +15,7 @@ use Compress::Zlib qw(compress inflateInit);
 use autouse 'Data::Dumper'   => qw(Dumper);
 use AutoLoader qw(AUTOLOAD);
 
-our $VERSION = '0.35_02';
+our $VERSION = '0.36';
 our @ISA     = qw(Exporter);
 our @EXPORT  = qw(prFile
                   prPage
