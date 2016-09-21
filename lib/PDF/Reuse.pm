1daba67c156b23e6da72c293e986e566317eed89
Chris Nighswonger <cnighswonger@foundations.edu>
2016-09-21 14:39:28 -0400

    Bumping revision to 0.36.04

--- lib/PDF/Reuse.pm
+++ lib/PDF/Reuse.pm
@@ -15,7 +15,7 @@ use Compress::Zlib qw(compress inflateInit);
 use autouse 'Data::Dumper'   => qw(Dumper);
 use AutoLoader qw(AUTOLOAD);
 
-our $VERSION = '0.36_03';
+our $VERSION = '0.36_04';
 our @ISA     = qw(Exporter);
 our @EXPORT  = qw(prFile
                   prPage
